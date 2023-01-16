3fc5b0a764f094257507122d3edc9d66f5d8094e
fiau <didan.mobe@gmail.com>
2023-01-16 15:09:55 +0700

    add code

--- starter-template/src/pages/Students/Students.tsx
+++ starter-template/src/pages/Students/Students.tsx
@@ -2,22 +2,30 @@ import { getStudent } from 'apis/student.api'
 import { Fragment, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Students } from 'types/student.type'
-
+import { useQuery } from '@tanstack/react-query'
+import { useSearchParams } from 'react-router-dom'
+import { useQueryString } from 'utils/utils'
 export default function StudentsPage() {
-  const [students, setStudents] = useState<Students>([])
-  const [isLoading, setIsLoading] = useState<Boolean>(false)
-  useEffect(() => {
-    setIsLoading(true)
-    getStudent(1, 10)
-      .then((res) => {
-        setStudents(res.data)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-      .finally(() => setIsLoading(false))
-  }, [])
+  // const [students, setStudents] = useState<Students>([])
+  // const [isLoading, setIsLoading] = useState<Boolean>(false)
+  // useEffect(() => {
+  //   setIsLoading(true)
+  //   getStudent(1, 10)
+  //     .then((res) => {
+  //       setStudents(res.data)
+  //     })
+  //     .catch((err) => {
+  //       console.log(err)
+  //     })
+  //     .finally(() => setIsLoading(false))
+  // }, [])
+  const queryString: { page?: string } = useQueryString()
+  const page = Number(queryString.page) || 1
 
+  const { data, isLoading } = useQuery({
+    queryKey: ['students', page],
+    queryFn: () => getStudent(page, 10)
+  })
   return (
     <div>
       <h1 className='text-lg'>Students</h1>
@@ -43,7 +51,7 @@ export default function StudentsPage() {
         <Fragment>
           <div className='relative mt-6 overflow-x-auto shadow-md sm:rounded-lg'>
             <table className='w-full text-left text-sm text-gray-500 dark:text-gray-400'>
-              <thead className='bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400'>
+              <thead className='bg-gray-50 text-xs uppercase text-gray-700  dark:text-gray-400'>
                 <tr>
                   <th scope='col' className='py-3 px-6'>
                     #
@@ -63,16 +71,19 @@ export default function StudentsPage() {
                 </tr>
               </thead>
               <tbody>
-                {students.map((student) => (
+                {data?.data.map((student) => (
                   <tr
                     key={student.id}
-                    className='border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600'
+                    className='border-b bg-white hover:bg-gray-50 dark:border-gray-700  dark:hover:bg-gray-600'
                   >
                     <td className='py-4 px-6'>{student.id}</td>
                     <td className='py-4 px-6'>
                       <img src={student.avatar} alt='student' className='h-5 w-5' />
                     </td>
-                    <th scope='row' className='whitespace-nowrap py-4 px-6 font-medium text-gray-900 dark:text-white'>
+                    <th
+                      scope='row'
+                      className='whitespace-nowrap  py-4 px-6 font-medium text-gray-900 dark:text-gray-700'
+                    >
                       {student.last_name}
                     </th>
                     <td className='py-4 px-6'>{student.email}</td>
