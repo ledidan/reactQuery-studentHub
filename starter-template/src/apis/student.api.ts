a3e57d28e14805b1c6aceead8db6aa2291710be4
ledidan <didan.mobe@gmail.com>
2023-01-16 10:58:09 +0700

    commit

---
+++ starter-template/src/apis/student.api.ts
@@ -0,0 +1,9 @@
+import { Students } from '../types/student.type'
+import http from 'utils/http'
+export const getStudent = (page: number | string, limit: number | string) =>
+  http.get<Students>('students', {
+    params: {
+      _page: page,
+      limit: limit
+    }
+  })
